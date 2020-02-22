58f6a577b582f911f22c96bef4d8bc7ce2d10c28
JonasRW <59603895+JonasRW@users.noreply.github.com>
2020-02-22 16:57:43 +0100

    Cleaned + save debug
    Cleaned version of path-1 with working image save function. Achieved with better float32 to unit8 conversion.

--- eCS_test.py
+++ eCS_test.py
@@ -11,6 +11,7 @@ from skimage.transform import rotate
 from fftFunction import fft
 from newROI import createROI
 from adaptedFrangi import frangi 
+import time
 
 def perform_eCS_algorithm(img_in, sigma_x = 1.0, sigma_y = 1.0, angle = 0.0, mask_fft = np.zeros((0))):
     """
@@ -43,6 +44,8 @@ def perform_eCS_algorithm(img_in, sigma_x = 1.0, sigma_y = 1.0, angle = 0.0, mas
 
     img_equalized = cv2.equalizeHist(img_in)
     img_roi = createROI(img_equalized)
+    plt.figure()
+    #plt.imshow(img_roi) - Good estimation of ROI. Implement in dataArtist
     img_smoothed= cv2.bilateralFilter(img_equalized,3,25,25)
     
     if angle != 0.0:
@@ -54,44 +57,18 @@ def perform_eCS_algorithm(img_in, sigma_x = 1.0, sigma_y = 1.0, angle = 0.0, mas
         img_y  = img_as_ubyte(frangi(img_smoothed, sigma_x = sigma_y, sigma_y = sigma_x, beta1=0.5, beta2=0.05,black_ridges=True))
         img_result = np.maximum(img_result,img_y)
 
-
-#Make work for rect. Needs to account for h and w
-
-    if angle != 0.0:    
+    if angle != 0.0: #Modified to handel rect img of diff sizes with np.shape    
         w = img_result.shape[0]
         w_org = img_in.shape[0] #changed to higt and width
         offset = (w - w_org)//2
-        #print(offset)
         h_org = img_in.shape[1]
         
-        #print(img_raw.shape[1])
-        #print(w_org)
-        #print(h_org)
-        print(img_result.shape)
-        #print(img_roi.shape)
-        #plt.imshow(img_result)
-        img_result = rotate(img_result,-angle,resize=True,preserve_range=True)[offset:offset+w_org,offset:offset+h_org] #make work for h and w!!!
-        print(img_result.shape) 
-        #plt.figure()
-        #img_rot = rotate(img_raw, 90)
-
-        #print(img_rot.shape)
-
-        
-#        shape = img_result.shape
-#        shape_org = img_in.shape
-#        w_offset = (shape[0]-shape_org[0])//2
-#        h_offset = (shape[1]-shape_org[1])//2
-#        img_result = rotate(img_result,-angle, preserve_range=True)[w_offset:w_offset+shape_org[0],h_offset:h_offset+shape_org[1]] #make work for h and w!!!
-        
-        
-        #For angels of rectangular pics
-        #img_roi = rotate(img_roi,-angle,resize=True, preserve_range=True)[offset:offset+h_org,offset:offset+w_org]
-        #print(img_roi.shape)
+        #img is rotated back, and scaled to right size
+        img_result = rotate(img_result,-angle,resize=True,preserve_range=True)[offset:offset+w_org,offset:offset+h_org] 
         
     img_result = img_result*img_roi
 
-    return img_result.astype('float32')
+    return img_result.astype('float32')#does this need to be float32? can unit8?
 
 
 '''
@@ -99,7 +76,7 @@ Main method to run the program
 '''
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    from skimage import data, img_as_float
+    from skimage import img_as_float
     from skimage.color import rgb2gray
     #Image path: change to load own images
     img_path = 'RawImages/maske+crop-savetest.png' #crack_image.tif
@@ -109,27 +86,31 @@ if __name__ == '__main__':
     
     #eCS parameter - if degree_of_asymmetry is 1.0, it is basically standard Frangi filtering with an isotropic kernel 
     sigma_a = 2.0
-    degree_of_asymmetry = 1.0
+    degree_of_asymmetry = 1.5
 
     #read files from paths
     mask_fft = io.imread(fft_mask_path)
     img_raw = io.imread(img_path)
     m = np.amax(img_raw)
-    #plt.figure(), plt.imshow(img_raw)
-    #plt.figure(), plt.imshow(mask_fft)
+    #Make sure they are gray scale and singel chanel so code can handle them.
     mask_fft = rgb2gray(img_as_float(mask_fft))
     img_raw = rgb2gray(img_as_float(img_raw))
-    #plt.figure(), plt.imshow(img_raw)
-    #plt.figure(), plt.imshow(mask_fft)
+
 
     # perform algorithm
     result = perform_eCS_algorithm(img_raw,sigma_x = sigma_a, sigma_y = degree_of_asymmetry*sigma_a, angle= 0.0, mask_fft=mask_fft)
     plt.figure()
     plt.imshow(result)
     plt.title('Sigma:{}'.format(sigma_a) + ' and DoAs:{}'.format(degree_of_asymmetry))
-    #Image to unit8 for image storage
     plt.savefig('plt_ecs_result.tif')
-    result = (result * m).round().astype(np.uint8)
+    
+    #Image to unit8 for image storage
+    #Does not give optimal image. might be dataloss when converted to 8bit.
+    #uint8 from fftFunction
+    result = cv2.normalize(result, 0, 255, cv2.NORM_MINMAX)
+    result *=255
+    result = np.uint8(result)
+    #result = (result * m).round().astype(np.uint8)
     io.imsave('ecs_result.tif', result)
     cv2.imwrite('cv2_ecs_result.tif',result)
 
